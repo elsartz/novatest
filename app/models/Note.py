ea0057c66e6decbdf1af6392e9812bf2bbe4867a
VSartz <elsartz@gmail.com>
2023-02-04 13:01:20 -0500

    able to insert bigger article

--- app/models/Note.py
+++ app/models/Note.py
@@ -1,11 +1,11 @@
 from app.db import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 
 class Note(Base):
   __tablename__ = 'notes'
   id = Column(Integer, primary_key=True)
   title = Column(String(80))
-  content = Column(String(250))
+  content = Column(Text)   # I run the cdm mysql> "alter table notes modify content TEXT;" to change the content column to TEXT 
 
   def __init__(self, title, content):
     self.title = title
